3214232775dc74c146e4638d9ec36403fd7bfb34
writtic <writtic@gmail.com>
2016-08-12 08:45:58 +0900

    Add & test ascoltatori

---
+++ ascoltatori/start.js
@@ -0,0 +1,54 @@
+// var ponte = require('ponte');
+var ascoltatori_mqtt = require('ascoltatori');
+var ascoltatori_kafka = require('ascoltatori');
+var sleep = require('sleep');
+var mqtt = require('mqtt');
+var arg_mqtt = null;
+var arrrg_kafka = null;
+var signal = 0;
+var signal2 = 0;
+var settings = {
+    type: 'mqtt',
+    json: false,
+    mqtt: require('mqtt'),
+    url: 'http://localhost:1883'
+};
+
+var settings_kafka = {
+    type: 'kafka',
+    json: false,
+    kafka: require('kafka-node'),
+    connectString: "localhost:2181",
+    clientId: "ascoltatorigroup",
+    groupId: "test-consumer-group",
+    defaultEncoding: "utf8",
+    encodings: {
+        image: "utf8"
+    }
+};
+
+function app() {
+    ascoltatori_mqtt.build(settings, function(err_mqtt, ascoltatori_mqtt) {
+        ascoltatori_kafka.build(settings_kafka, function(err_kafka, ascoltatore_kafka) {
+            ascoltatori_mqtt.subscribe("test", function() {
+                console.log("mqtt_subscribe...\n" + "value: " + arguments[1] + ".....\n");
+                arg_mqtt = arguments[1];
+                ascoltatore_kafka.publish("test", arg_mqtt, function() {
+                    console.log("kafka_publish...\n" + "value: " + arg_mqtt + ".....\n");
+                    arg_mqtt = null;
+                    ascoltatore_kafka.subscribe("messages", function() {
+                        console.log("kafka_subscribe...\n" + "value: " + arguments[1] + "\n");
+                        arrrg_kafka = arguments[1];
+                        ascoltatori_mqtt.publish("messages", arrrg_kafka, function() {
+                            console.log("mqtt_publish...\n" + "value: " + arrrg_kafka + "\n");
+                            arrrg_kafka = null;
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+if (require.main === module) {
+    app();
+}
